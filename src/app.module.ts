3cd4197b03c3d65788b0ba4a710035c71952ec20
Manish Kumar Sarangi <manish.sarangi@xseededucation.com>
2024-06-12 12:49:14 +0530

    Added config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
 import { QuizModule } from './quiz/quiz.module';
 
 @Module({
-  imports: [AuthModule, QuizModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.QUIZ_DB_CONNECTION!),
+    AuthModule,
+    QuizModule
+  ],
 })
 export class AppModule { }
